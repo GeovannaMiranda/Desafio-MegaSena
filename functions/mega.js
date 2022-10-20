86f9d27fd7c0c9731c7b640a25118e990a002408
GeovannaMiranda <Geovannanm564@outlook.com>
2022-10-20 14:24:36 -0300

    Desafio Mega sena

---
+++ functions/mega.js
@@ -0,0 +1,20 @@
+export function mega(quantidade = 6, numeros = []){
+    let qtde = +quantidade
+    
+    if(qtde < 6 && qtde > 60){
+        throw "Quantidade inválida!"
+    }
+
+    if(numeros.length === qtde){
+        return numeros.sort((n1, n2) => n1 - n2) //ordenando de forma crescente
+    }
+
+    const numeroAleatorio = parseInt(Math.random() * 60) + 1
+    if(!numeros.includes(numeroAleatorio)){
+        return mega(qtde, [...numeros, numeroAleatorio])  //...numeros - uma forma de clonar o array 
+    }else{
+        return mega(qtde, numeros)
+    }
+}
+
+//console.log("Resultado mega", mega())
